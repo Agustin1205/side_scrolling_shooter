408ec251d25c6a5fb50c26e657c5e8db9e746a7a
Agustin1205 <115505110+Agustin1205@users.noreply.github.com>
2024-04-29 13:34:29 -0300

    juego terminado

--- Assets/Scripts/Controller_PowerUp.cs
+++ Assets/Scripts/Controller_PowerUp.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller_PowerUp : Projectile
+public class PowerUpController : MonoBehaviour
 {
+    public float invisibleDuration = 5f; // Duración de la invisibilidad
+    private bool consumed = false; // Estado de si ha sido consumido o no
     private Rigidbody rb;
 
     private void Start()
@@ -13,6 +15,37 @@ public class Controller_PowerUp : Projectile
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(-0.7f,0,0);
+        // Movimiento del power-up
+        rb.velocity = new Vector3(-0.7f, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !consumed)
+        {
+            // Desactivar el renderizador del jugador
+            Renderer playerRenderer = other.GetComponent<Renderer>();
+            if (playerRenderer != null)
+            {
+                playerRenderer.enabled = false;
+            }
+
+            // Establecer una corrutina para hacer visible al jugador después de la duración de la invisibilidad
+            StartCoroutine(MakePlayerVisibleAfterDelay(other.gameObject));
+            consumed = true;
+        }
+    }
+
+    private IEnumerator MakePlayerVisibleAfterDelay(GameObject player)
+    {
+        // Esperar la duración de la invisibilidad
+        yield return new WaitForSeconds(invisibleDuration);
+
+        // Activar el renderizador del jugador nuevamente
+        Renderer playerRenderer = player.GetComponent<Renderer>();
+        if (playerRenderer != null)
+        {
+            playerRenderer.enabled = true;
+        }
     }
 }
--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    
-    void Update()
+    private void Update()
     {
-        GetInput();
+        CheckForRestartInput();
     }
 
-    private void GetInput()
+    private void CheckForRestartInput()
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Controller_Player._Player.gameObject.SetActive(true);
-            Time.timeScale = 1;
-            SceneManager.LoadScene(0);
+            RestartLevel();
         }
+    }
+
+    private void RestartLevel()
+    {
+        // Reactivar al jugador si está desactivado
+        if (!Controller_Player._Player.gameObject.activeSelf)
+        {
+            Controller_Player._Player.gameObject.SetActive(true);
+        }
+
+        // Reanudar el tiempo si se pausó
+        Time.timeScale = 1f;
+
+        // Reiniciar la escena actual
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
